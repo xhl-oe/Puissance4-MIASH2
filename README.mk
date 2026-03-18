5bcf39d8a2b3e223dffc27fb619308b2056cdfd9
jehane-moummi <moummijehane@gmail.com>
2026-03-19 00:02:13 +0100

    Update README.mk

--- README.mk
+++ README.mk
@@ -1,1 +1,19 @@
+#Création de la grille 6 lignes * 7 colonnes 
+lignes = 6 
+colonnes = 7 
+grille = []
 
+for i in range(lignes) : 
+  ligne = []
+  for j in range(colonnes) : 
+    ligne.append(" ") 
+  grille.append(ligne) 
+
+#fonction pour afficher la grille 
+def afficher_grille() : 
+  for ligne in grille : 
+    print("|", end=" ")
+    for case in ligne : 
+      print(case, end= " | ")
+    print()
+  print("-" * (colonnes * 4)) 
