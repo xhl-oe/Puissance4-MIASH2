7df049e169490f0cda5ad28a085a9b3076c30ce5
xhl-oe <aurelie.meno92@gmail.com>
2026-05-06 21:21:37 +0200

    README.mk

--- README.mk
+++ README.mk
@@ -1,1 +1,12 @@
-groupe puiss4
+Le jeu Puissance 4 est un jeu réalisé par Khery, Jehane, Chloé et Amine, des étudiants de L1 MIASHS. 
+C'est un jeu qui se joue à deux joueurs, chaque joueur a une couleur. Il y a une grille verticale qui possèdent 7 colonnes et 6 lignes.
+
+COMMENT CE JEU SE DEROULE-T-IL ? 
+A tour de rôle, les joueurs mettent un jeton qu'ils auront choisi. Le jeton se pose soit en bas de la grille soit sur un jeton. 
+
+LE BUT DU JEU 
+Pour gagner, il faut aligner 4jetons soit :
+ - horizontalement 
+ - verticalement
+ - en diagonale 
+
