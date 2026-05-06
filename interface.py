ef173e8f7fc4f6b84be1abb65a643e4a8aa82231
jehane-moummi <moummijehane@gmail.com>
2026-05-06 13:23:05 +0200

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,83 +1,57 @@
-#a l'aide du cours en ligne OrdiRoutier(sur YouTube)on cree une inetrface
-import tkinter as tk
+#JEHANE
+# Grille graphique
+grille_graphique = []
 
-fenetre = tk.Tk()
+canvas = tk.Canvas(fenetre,width=420, height=360, bg="white")#Largeur420 pixels.Hauteur du canvas = 360 pixels
+# CREATION DE LA GRILLE
+def creer_grille():
+    global grille_graphique
+    grille_graphique = []
+    canvas.delete("all")
+    for ligne in range(lignes):
+        ligne_tab = []
+        for col in range(colonnes):
+            x1 = col * taille_case# x1=0 pour la première colonne, x1=60 pour la deuxième colonne, etc.
+            y1 = ligne * taille_case# y1=0 pour la première ligne, y1=60 pour la deuxième ligne, etc.
+            x2 = x1 + taille_case # x2=60 pour la première colonne, x2=120 pour la deuxième colonne, etc.
+            y2 = y1 + taille_case # y2=60 pour la première ligne, y2=120 pour la deuxième ligne, etc.
 
-fenetre.geometry("560x480")  # correction ici
-fenetre.title("Puissance 4")
-#fenetre.["zffedser"]="red"
-#fenetre.resizable(height=False,width=False)
+            case = canvas.create_oval(x1,y1,x2,y2,fill="lightpink", outline="black")
+            ligne_tab.append(case)
+        grille_graphique.append(ligne_tab)
 
-label=tk.Label(fenetre,text="Puissance 4♥",font=("verdana",24,"italic"),fg="PaleVioletRed",bg="pink")#PaleVioletRed=#DB7093
-#label.pack(side=RIGHT,padx=10)
-label.pack() 
-sub_label=tk.Label(fenetre,text="by Khery,Jehane,Chloe,Amine",font=("verdana",12,"italic"),fg="PaleVioletRed",bg="pink")
-sub_label.pack()
-#sub_label["texte"]="nimporte quel texte"#pour changer le texte de sub_label comme pour une liste
-#def jouer():
-  
-def bouton_jouer():
-    print("le jeu commenceavec {nom1} et {nom2}")
-bouton = tk.Button(fenetre, text="Jouer", font=("verdana", 30, "italic"), fg="PaleVioletRed", bg="pink", command=bouton_jouer,padx=70)
-bouton.pack(pady=100)
+def grille_pleine():
 
-def jouer():
-    nom_joueur1 = entree1.get()
-    nom_joueur2 = entree2.get()
-    if not nom_joueur1 or not nom_joueur2:
-        print("Veuillez entrer deux pseudos")
-        return
-    if nom_joueur1 == nom_joueur2:
-        print("Pseudo déjà pris")
-        return
+    for ligne in grille_logique:
+        if 0 in ligne:
+            return False
 
-    print(f"Le joueur {nom_joueur1} est rentré dans le jeu")
-    print(f"Le joueur {nom_joueur2} est rentré dans le jeu")
+    return True
 
-nom_joueur1 = tk.StringVar()
-nom_joueur2 = tk.StringVar()
-label1=tk.Label(fenetre,text="Entrez pseudo joueur 1",font=("verdana",12,),fg="PaleVioletRed",bg="pink")
-label1.pack()
+# VERIFICATION VICTOIRE
+def verifier_victoire(joueur):
 
-entree1=tk.Entry(fenetre,textvariable= nom_joueur1,font=("verdana",12,),fg="PaleVioletRed",bg="pink")
-entree1.pack()
+    # Horizontal
+    for l in range(lignes):
+        for c in range(colonnes - alignement_gagnant + 1):
+            if all(grille_logique[l][c+i] == joueur for i in range(alignement_gagnant)):
+                return True
 
-label2=tk.Label(fenetre,text="Entrez pseudo joueur 2",font=("verdana",12,),fg="PaleVioletRed",bg="pink")
-label2.pack()
+    # Vertical
+    for l in range(lignes - alignement_gagnant + 1):
+        for c in range(colonnes):
+            if all(grille_logique[l+i][c] == joueur for i in range(alignement_gagnant)):
+                return True
+    # Diagonale descendante
+    for l in range(lignes - alignement_gagnant + 1):
+        for c in range(colonnes - alignement_gagnant + 1):
+            if all(grille_logique[l+i][c+i] == joueur for i in range(alignement_gagnant)):
+                return True
 
-entree2=tk.Entry(fenetre,textvariable=nom_joueur2,font=("verdana",12,),fg="PaleVioletRed",bg="pink")
-entree2.pack()
+    # Diagonale montante
+    for l in range(alignement_gagnant - 1, lignes):
+        for c in range(colonnes - alignement_gagnant + 1):
+            if all(grille_logique[l-i][c+i] == joueur for i in range(alignement_gagnant)):
+                return True
 
-bouton=tk.Button(fenetre,text="Valider",font=("verdana",12,),fg="PaleVioletRed",bg="pink",command=jouer)
-bouton.pack()
-
-mon_menu = tk.Menu(fenetre)
-
-#les sous onglets
-Jouer=tk.Menu(mon_menu,tearoff=0)
-Jouer.add_command(label="jouer en 1v1")
-Jouer.add_command(label="jouer contre l'ia")
-
-Quitter=tk.Menu(mon_menu,tearoff=0)
-Quitter.add_command(label="Quitter le jeux")
-
-Aide=tk.Menu(mon_menu,tearoff=0)#tearoff=0 pour ne pas pouvoir detacher le menu
-Aide.add_command(label="Règles du jeux")
-Aide.add_command(label="Menu d'aide")
-Aide.add_command(label="Modifier les paramètres du jeux")
-#Les menues du jeux
-mon_menu.add_cascade(label="Jouer                                                                    ",menu=Jouer)
-mon_menu.add_cascade(label="Aide                                                                    ",menu=Aide)
-mon_menu.add_cascade(label="Quitter",menu=Quitter)
-
-fenetre.config(menu=mon_menu) #pour afficher le menu dans la fenetre
-
-
-
-
-
-
-
-fenetre.mainloop() #boucle pour afficher la fenetre et la maintenir ouverte
-
-#test khery
+    return False
